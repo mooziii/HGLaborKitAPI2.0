aa7468db81cd0718d49645b631b6980a5373ac85
royzer <royzerfisch@gmail.com>
2021-06-25 19:00:10 +0200

    update scout

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/ScoutKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/ScoutKit.java
@@ -3,6 +3,7 @@ package de.hglabor.plugins.kitapi.kit.kits;
 import com.google.common.collect.ImmutableMap;
 import de.hglabor.plugins.kitapi.KitApi;
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.kit.events.KitEvent;
 import de.hglabor.plugins.kitapi.kit.settings.IntArg;
 import de.hglabor.plugins.kitapi.kit.settings.PotionEffectArg;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
@@ -12,6 +13,8 @@ import de.hglabor.utils.noriskutils.TimeConverter;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
@@ -48,15 +51,16 @@ public class ScoutKit extends AbstractKit {
     
     @KitEvent
     @Override
-    public void onEntityDamage(EntityDamageEvent event){
-        Player player = event.getEntity();
-        if(!event.getCause().equals(EntityDamageEvent.DamageCause.FALL))return;
-        if (player.hasPotionEffect(PotionEffectType.SPEED)){
-            if(event.getFinalDamage() > 4){
+    public void onEntityDamage(EntityDamageEvent event) {
+        Player player = (Player) event.getEntity();
+        if (!event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) return;
+        if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+            if (event.getFinalDamage() > 4) {
                 event.setDamage(4);
             }
         }
     }
+
 
     @Override
     public void onEnable(KitPlayer kitPlayer) {
